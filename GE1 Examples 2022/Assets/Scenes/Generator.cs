925049afbb217df73e5a2abd9d61557e4da91c1a
NterpriseCEO <nuzumalex@gmail.com>
2022-10-04 00:53:07 +0100

    Added the dodecahedron circle lab

---
+++ GE1 Examples 2022/Assets/Scenes/Generator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+	[SerializeField] GameObject prefab;
+
+	[SerializeField] int loops = 5;
+	int maxLoops;
+	int n = 0;
+	float radius;
+	float circumference;
+	// Start is called before the first frame update
+	void Start() {
+
+		maxLoops = loops;
+
+		while(radius >= 0) {
+			loops--;
+			radius = loops/2;
+			circumference = 2 * Mathf.PI * radius;
+			n = (int) (circumference / (2*0.5));
+			float s = calculateS();
+
+			while(n > 1 && s < 2*0.5f) {
+				n--;
+				s = calculateS();
+			}
+
+			for (int i = 0; i < n; ++i) {
+				float angle = Mathf.PI * 2f * i / n;
+				GameObject dd = GameObject.Instantiate(prefab, new Vector3(radius*Mathf.Cos(angle), 0f, radius * Mathf.Sin(angle)), Quaternion.identity);
+				dd.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.HSVToRGB(1f/(maxLoops-loops), 1f, 1f);
+			}
+
+			radius--;
+		}
+	}
+
+	float calculateS() {
+		return 2*radius*Mathf.Sin(Mathf.PI/n);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
---
+++ GE1 Examples 2022/Assets/Scenes/RotateMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour {
+	[Range(0, 360)]
+	float speed = 90;
+	Renderer dodecahedron;
+	float H, S, V;
+	// Start is called before the first frame update
+	void Start() {
+		//get child by name of this prefab
+		dodecahedron = GetComponentsInChildren<Renderer>()[0];
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.Rotate(Time.deltaTime*speed, Time.deltaTime*speed, -Time.deltaTime*speed);
+		Color.RGBToHSV(dodecahedron.material.color, out H, out S, out V);
+
+		dodecahedron.material.color = Color.HSVToRGB(H+=0.001f, S, V);
+	}
+}
